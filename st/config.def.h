dabf951c3b3062aa9ff2b3eec5852b2dd5aefefd
Alexandr Sychev <alx.sychevvv@gmail.com>
2024-02-12 23:41:35 +0300

    .

--- st/config.def.h
+++ st/config.def.h
@@ -94,7 +94,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.8;
+float alpha = 0.95;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
@@ -196,8 +196,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
-	{ ShiftMask,            XK_U,           kscrollup,      {.i = -1} },
-	{ ShiftMask,            XK_D,           kscrolldown,    {.i = -1} },
+	{ TERMMOD,              XK_U,           kscrollup,      {.i = -1} },
+	{ TERMMOD,              XK_D,           kscrolldown,    {.i = -1} },
 };
 
 /*
